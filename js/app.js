c716ff42012ce31ac4974a801f20ff437de8b623
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-22 20:28:25 -0300

    Corrige zoom vertical e fallback interno v0.3.12

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const $ = (s, root = document) => root.querySelector(s);
 const $$ = (s, root = document) => [...root.querySelectorAll(s)];
 
 const CONFIG = {
-  appVersion: '0.3.1',
+  appVersion: '0.3.12',
   folderIds: [],
   folderUrls: [],
   folderId: '',
