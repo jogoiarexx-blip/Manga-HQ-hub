12364867acadf0af0a35449250f0bf3dab864d8a
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-23 09:16:40 -0300

    Atualiza cache PWA para v0.3.19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.3.18';
+const APP_VERSION = '0.3.19';
 const CACHE_PREFIX = 'manga-hq-hub-ghpages-';
 const LEGACY_CACHE_PREFIXES = ['manga-hq-reader-ghpages-'];
 const CACHE = `${CACHE_PREFIX}v${APP_VERSION}`;
