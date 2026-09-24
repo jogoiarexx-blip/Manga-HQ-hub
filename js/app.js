8255f5b561886fae2cacb77ed78f43300487c661
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-24 16:50:57 -0300

    Atualizar versão interna do Hub para v0.3.25

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const $ = (s, root = document) => root.querySelector(s);
 const $$ = (s, root = document) => [...root.querySelectorAll(s)];
 
 const CONFIG = {
-  appVersion: '0.3.24',
+  appVersion: '0.3.25',
   folderIds: [],
   folderUrls: [],
   folderId: '',
